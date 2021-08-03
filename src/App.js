5da3add1ec2c78137852053b5036b78410d8113c
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-08-03 23:14:03 +0100

    How we can change state for inputText

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
+import React, {useState} from 'react';
 //importing components
 import Form from './components/Form'
 import TodoList from './components/TodoList'
 
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+
   return (
     <div className="App">
      <header>
        <h1>Aleks Todo List</h1>
      </header>
-     <Form />
+    <Form setInputText={setInputText}/>
     <TodoList />
+    <p>{inputText}</p>
     </div>
   );
 }
